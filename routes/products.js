9c7cc80266f2c802abdfe36207d97942a5ad5860
unknown <22100535@ibsu.edu.ge>
2024-06-25 00:07:49 +0400

    initial commit

--- routes/products.js
+++ routes/products.js
@@ -2,11 +2,20 @@ const express = require('express');
 const router = express.Router();
 
 const productService = require('../services/productService');
+const ApiSecurity = require('../middleware/apiSecurity');
 
 router.get('/all', productService.getAll);
 router.get('/:id', productService.getOne);
 router.post('/add', productService.add);
 router.put('/:id', productService.update);
 router.post('/search', productService.search);
+
+/*
+router.get('/all', ApiSecurity.requireLogin, productService.getAll);
+router.get('/:id', ApiSecurity.requireLogin, productService.getOne);
+router.post('/add', ApiSecurity.requirePermits('manage_product'), productService.add);
+router.put('/:id', ApiSecurity.requirePermits('manage_product'), productService.update);
+router.post('/search', ApiSecurity.requireLogin, productService.search);
+*/
 
 module.exports = router;
